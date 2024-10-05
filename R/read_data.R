17a144ce396a6c7ba40d779aff2a4dc7b949c810
Richard Lindsey <richard.c.lindsey@ricompute.com>
2024-10-05 12:17:26 -0500

    Add simple documentation to read_key_csv()

--- R/read_data.R
+++ R/read_data.R
@@ -1,3 +1,13 @@
+#' Read in a sample definition key file
+#'
+#' @param file The file path to the `key.csv` file.
+#' @param ... Additional arguments passed on to `readr::read_csv()`.
+#'
+#' @return A tibble/data frame containing the sample definitions.
+#' @export
+#'
+#' @examples
+#' key <- read_key_csv("data/key.csv")
 read_key_csv <- function(file, ...) {
     readr::read_csv(file, ...) |>
         tidyr::pivot_longer(cols = c("Spine", "Met", "Dia"),
