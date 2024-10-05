11c35292b8c25f0311dcfc48827a70e5dcc2bb2c
Richard Lindsey <richard.c.lindsey@ricompute.com>
2024-10-05 14:56:00 -0500

    Add example key data with helper function to get file path. Use for read_key_csv example.

---
+++ R/example_data.R
@@ -0,0 +1,21 @@
+#' Get path to microCTr examples
+#'
+#' microCTr comes bundled with some example files in its `inst/extdata`
+#' directory. This function makes them easy to access.
+#'
+#' @param path Name of file. If `NULL`, the example files will be listed.
+#'
+#' @return A string containing the name of the file, or a character vector
+#'   containing the names of all example files.
+#' @export
+#'
+#' @examples
+#' microCTr_example()
+#' microCTr_example("example-key.csv")
+microCTr_example <- function(path = NULL) {
+    if (is.null(path)) {
+        dir(system.file("extdata", package = "microCTr"))
+    } else {
+        system.file("extdata", path, package = "microCTr", mustWork = TRUE)
+    }
+}
--- R/read_data.R
+++ R/read_data.R
@@ -1,16 +1,35 @@
 #' Read in a sample definition key file
 #'
+#' This is a wrapper around `readr::read_csv()` to read in and process
+#' a `key.csv` file that contains the lookup table defining each sample's
+#' AS (key registry) number, sex, genotype (if applicable), treatment (if
+#' applicable), sample number, and the measure number of each site scanned.
+#'
 #' @param file The file path to the `key.csv` file.
+#' @param sites A character vector of the sites scanned, included as columns in
+#'   the `key.csv`.
 #' @param ... Additional arguments passed on to `readr::read_csv()`.
 #'
 #' @return A tibble/data frame containing the sample definitions.
+#'   For example:
+#'   ```
+#'   |   AS|Sex |Genotype | SampNo|Site  | MeasNo|
+#'   |----:|:---|:--------|------:|:-----|------:|
+#'   | 1365|M   |Cre      |  10778|Spine |  31711|
+#'   | 1365|M   |Cre      |  10778|Met   |  31710|
+#'   | 1365|M   |Cre      |  10778|Dia   |  31712|
+#'   | 1366|F   |Cre      |  10779|Spine |  31714|
+#'   | 1366|F   |Cre      |  10779|Met   |  31713|
+#'   | 1366|F   |Cre      |  10779|Dia   |  31715|
+#'   ```
+#'
 #' @export
 #'
 #' @examples
-#' key <- read_key_csv("data/key.csv")
-read_key_csv <- function(file, ...) {
+#' key <- read_key_csv(microCTr_example("example-key.csv"))
+read_key_csv <- function(file, sites = c("Spine", "Met", "Dia"), ...) {
     readr::read_csv(file, ...) |>
-        tidyr::pivot_longer(cols = c("Spine", "Met", "Dia"),
+        tidyr::pivot_longer(cols = sites,
                             names_to = "Site",
                             values_to = "MeasNo")
 }
